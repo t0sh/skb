8403460a1a78de81648427aed7130540ccf58b7f
Igor Suvorov <suvorov.igor.90@gmail.com>
2016-11-13 14:36:56 +0400

    Api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import config from './config';
+import App from './App';
 
-const app = express();
-app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
-});
-
-app.listen(3000, () => {
-  console.log('Your app listening on port 3000!');
-});
+const app = new App({ config });
+app.run();
