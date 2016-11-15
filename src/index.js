17bfd25a924ed49974e3c705b506bc284b6f4d86
t0sh <t0sh@mail.ru>
2016-11-15 13:47:55 +0500

    pockemons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,66 @@
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
+import fetch from 'isomorphic-fetch';
+import Promise from 'bluebird';
+import _ from 'lodash';
+
+const _DEV_ = true;
 
 const app = express();
-app.use(cors());
-app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+//app.use(cors());
+
+const baseUrl = 'https://pokeapi.co/api/v2';
+const pokemonFields = ['id', 'name', 'base_experience', 'is_default', 'order', 'weight'];
+
+async function getPokemons(url, i = 0) {
+  console.log('getPokemons ', url, i);
+  const response = await fetch(url);
+  const page = await response.json();
+  const pokemons = page.results;
+  if (_DEV_ && i > 1) {
+    return pokemons;
+  }
+  if (page.next) {
+    const pokemons2 = await getPokemons(page.next, i + 1);
+    return [
+      ...pokemons,
+      ...pokemons2
+    ]
+  }
+  return pokemons;
+}
+
+async function getPokemon(url) {
+  console.log('getPokemon ', url);
+  const response = await fetch(url);
+  const pokemon = await response.json();
+  return pokemon;
+}
+
+
+app.get('/', async (req, res) => {
+  try{
+
+    const pokemonsUrl = ${baseUrl}/pokemon;
+//    const pokemonsUrl = https://pokeapi.co/api/v2/pokemon;
+
+    const pokemonsInfo = await getPokemons(pokemonsUrl);
+    const pokemonsPromises = pokemonsInfo.slice(0, 2).map( info =>{
+      return getPokemon(info.url);
+    });
+
+    const pokemonsFull = await Promise.all(pokemonsPromises);
+    const pokemons = pokemonsFull.map(pokemon => {
+      return _.pick(pokemon, pokemonFields);
+    })
+
+    return  res.json({
+      pokemons,
+    });
+  } catch(err) {
+    console.log(err);
+    return  res.json({ err });
+  }
 });
 
 app.listen(3000, () => {
