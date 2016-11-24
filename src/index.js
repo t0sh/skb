3325c57ccce78d059d3ccde676f8fb5c580ca0a3
t0sh <t0sh@mail.ru>
2016-11-24 20:23:53 +0500

    mongodb

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ mongoose.connect('mongodb://publicdb.mgbeta.ru/t0sh_skb3');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
 // app.use(isAdmin);
 
 app.get('/clear', isAdmin, async (req, res) => {
