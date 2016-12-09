ae67de78bd70891d4eff2287afca15065707fb5e
t0sh <t0sh@mail.ru>
2016-12-09 21:54:46 +0500

    52 test err

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+// TODO: В запросе users/populate?havePet=cat у юзеров приходят только коты, а должны приходить все животные, которые есть у держателей котов.
+
 import express, { Router } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import fetch from 'isomorphic-fetch';
-import config from './config';
 import _ from 'lodash';
+import config from './config';
 
 const app = express();
 
@@ -33,6 +35,32 @@ const rootReq = (req, res, next) => {
   res.json(petsData);
 };
 
+const reqPets = (req, res, next) => {
+  let pets = petsData.pets;
+  if (!pets) next('!pets');
+
+  const typePet = req.query.type || req.query.havePet;
+  if (typePet) {
+    pets = pets.filter(pet => (pet.type === typePet));
+    if (!pets) next('No pet type of ', typePet);
+  }
+
+  if (req.query.age_gt) {
+    const age_gt = req.query.age_gt;
+    pets = pets.filter(pet => (pet.age > age_gt));
+    if (!pets) next('No pet older than ', age_gt);
+  }
+
+  if (req.query.age_lt) {
+    const age_lt = req.query.age_lt;
+    pets = pets.filter(pet => (pet.age < age_lt));
+    if (!pets) next('No pet younger than ', age_lt);
+  }
+  req.pets = pets;
+
+  next();
+};
+
 const reqUsers = (req, res, next) => {
   const users = petsData.users;
   if (!users) next('!users');
@@ -44,11 +72,21 @@ const reqUserById = (req, res, next) => {
   const id = +req.params.id;
   if (!id) next('!id');
   const user = _.find(req.users, usr => (usr.id === id));
-  if (!user) next('!user')
+  if (!user) next('!user');
   req.user = user;
   next();
   // (req.user = req.users.filter(usr => (usr.id === id))) ? next() : next('!user');
 };
+
+const resUsersHasPetType = (req, res, next) => {
+  const users = req.users;
+  const pets = req.pets;
+  let usersHasPetType = pets.map(pet =>
+    _.find(users, user => (user.id === pet.userId)));
+  if (!usersHasPetType) next('!usersHasPet');
+  usersHasPetType = _(usersHasPetType).sortBy(['id']).sortedUniq();
+  res.json(usersHasPetType);
+}
 
 const reqUserByName = (req, res, next) => {
   const username = req.params.username;
@@ -59,12 +97,6 @@ const reqUserByName = (req, res, next) => {
   next();
 };
 
-const reqPets = (req, res, next) => {
-  const pets = petsData.pets;
-  if (!pets) next('!pets');
-  req.pets = pets;
-  next();
-};
 
 const reqPetById = (req, res, next) => {
   const id = +req.params.id;
@@ -84,116 +116,150 @@ const resPetsByUser = (req, res, next) => {
   res.json(petsByUser);
 };
 
-const resPetsByType = (req, res, next) => {
-  const pets = req.pets;
-  const typePet = req.query.type || req.query.havePet;
-  if (!typePet) next('!typePet');
-  const petsByType = pets.filter(pet => (pet.type === typePet));
-  if (!petsByType) next('!petsByType');
-  req.petsByType = petsByType;
-  next();
-}
-
-const resUsersHasPetType = (req, res, next) => {
+const petsPopulate = (req, res, next) => {
   const users = req.users;
   const pets = req.pets;
-  const petsByType = req.petsByType;
-  const usersHasPetType = petsByType.map(pet => users.filter(user => (user.id === pet.userId)));
-  if (!usersHasPetType) next('!usersHasPet');
-  res.json(usersHasPetType);
-}
-
-const resPetsAgeMoreThen = (req, res, next) => {
-  const pets = req.pets;
-  const age_gt = +req.query.age_gt;
-  const petsAgeMoreThen = pets.filter(pet => (pet.age > age_gt));
-  if (!petsAgeMoreThen) next('!petsAgeMoreThen');
-  res.json(petsAgeMoreThen);
+  const petsPop = pets.map((pet) => {
+    let newPet = _.clone(pet);
+    newPet['user'] = _.find(users, user => (user.id === pet.userId));
+    return newPet;
+  });
+  if (!petsPop) next('!petsPop');
+  res.json(petsPop);
 };
 
-const resPetsAgeLessThen = (req, res, next) => {
+const petPopulate = (req, res, next) => {
+  const users = req.users;
+  let pet = _.clone(req.pet);
+  const user = _.find(users, user => (user.id === pet.userId));
+  if (!user) next('!user');
+  pet.user = user;
+  res.json(pet);
+};
+
+const usersPopulate = (req, res, next) => {
+  const users = req.users;
   const pets = req.pets;
-  const age_lt = +req.query.age_lt;
-  const petsAgeLessThen = pets.filter(pet => (pet.age < age_lt));
-  if (!petsAgeLessThen) next('!petsAgeLessThen');
-  res.json(petsAgeLessThen);
-}
+  const usersPop = users.reduce((usersP, user) => {
+    let newUser = _.clone(user);
+    newUser['pets'] = pets.filter(pet => (user.id === pet.userId));
+    if (newUser.pets.length > 0)
+      usersP.push(newUser);
+    return usersP;
+  }, []);
+
+  if (!usersPop) next('!usersPop');
+  res.json(usersPop);
+};
+
+const userPopulate = (req, res, next) => {
+  let user = _.clone(req.user);
+  let pets = req.pets;
+  user.pets = pets.filter(pet => (user.id === pet.userId));
+  if (user.pets.length === 0)
+    next('user does not have pet');
+  res.json(user);
+};
+// const reqPetsByType = (req, res, next) => {
+//   const pets = req.pets;
+//   console.log('test');
+//   const typePet = req.query.type || req.query.havePet;
+//   if (!typePet) next('!typePet');
+//   const petsByType = pets.filter(pet => (pet.type === typePet));
+//   if (!petsByType) next('!petsByType');
+//   req.petsByType = petsByType;
+//   next();
+// }
+
+// const resUsersHasPetType = (req, res, next) => {
+//   const users = req.users;
+//   const pets = req.pets;
+//   const petsByType = req.petsByType;
+//   const usersHasPetType = petsByType.map(pet => _.find(users, user => (user.id === pet.userId)));
+//   if (!usersHasPetType) next('!usersHasPet');
+//   res.json(usersHasPetType);
+// }
+//
+// const resPetsAgeMoreThen = (req, res, next) => {
+//   const pets = req.pets;
+//   const age_gt = +req.query.age_gt;
+//   const petsAgeMoreThen = pets.filter(pet => (pet.age > age_gt));
+//   if (!petsAgeMoreThen) next('!petsAgeMoreThen');
+//   res.json(petsAgeMoreThen);
+// };
+//
+// const resPetsAgeLessThen = (req, res, next) => {
+//   const pets = req.pets;
+//   const age_lt = +req.query.age_lt;
+//   const petsAgeLessThen = pets.filter(pet => (pet.age < age_lt));
+//   if (!petsAgeLessThen) next('!petsAgeLessThen');
+//   res.json(petsAgeLessThen);
+// }
 
 const catchErrors = (err, req, res, next) => {
   res.status(404).send('Not Found');
 }
 
-// const entityRouter = express.Router();
 const rootRouter = Router();
-const usersRouter = Router();
-const userIdRouter = Router({ mergeParams: true });
-const petIdRouter = Router({ mergeParams: true });
-const userNameRouter = Router({ mergeParams: true });
-const petsRouter = Router();
-const petsByUserRouter = Router({ mergeParams: true });
-const usersHasPetTypeRouter = Router({ mergeParams: true });
-
 rootRouter.use(catchErrors);
-
 rootRouter.route('/')
   .get((req, res) => res.json(petsData));
 
-rootRouter.use('/users', usersRouter, logParams);
-
+const usersRouter = Router();
+rootRouter.use('/users', usersRouter);
 usersRouter.use(reqUsers);
-
 usersRouter.route('/')
-  .get((req, res) => res.json(req.users));
+  .get((req, res, next) => {
+    (req.query.havePet) ? next() :
+      res.json(req.users);
+  })
+  .get(reqPets, resUsersHasPetType);
 
-usersRouter.use('/:id(\\d+)', userIdRouter);
 
+const userIdRouter = Router({ mergeParams: true });
+usersRouter.use('/:id(\\-?\\d+)', userIdRouter);
 userIdRouter.use(reqUserById);
-
 userIdRouter.route('/')
   .get((req, res) => res.json(req.user));
 
-userIdRouter.use('/pets', petsByUserRouter);
+const usersPopulateRouter = Router();
+usersRouter.use('/populate', usersPopulateRouter);
+usersPopulateRouter.get('/', reqPets, usersPopulate);
 
-petsByUserRouter.use(reqPets);
-
-petsByUserRouter.use('/', resPetsByUser);
-
+const userNameRouter = Router({ mergeParams: true });
 usersRouter.use('/:username(\\w+)', userNameRouter);
-
 userNameRouter.use(reqUserByName);
-
 userNameRouter.route('/')
   .get((req, res) => res.json(req.user));
 
+const petsByUserRouter = Router();
+userIdRouter.use('/pets', petsByUserRouter);
 userNameRouter.use('/pets', petsByUserRouter);
+petsByUserRouter.get('/', reqPets, resPetsByUser);
 
-usersRouter.use('/?havePet', usersHasPetTypeRouter);
-
-usersHasPetTypeRouter.use(resUsersHasPetType);
-
-usersHasPetTypeRouter.route('/')
-  .get((req, res) => res.json());
-
+const petsRouter = Router();
 rootRouter.use('/pets', petsRouter);
-
 petsRouter.use(reqPets);
-
 petsRouter.route('/')
   .get((req, res) => res.json(req.pets));
 
-petsRouter.use('/:id(\\d+)', petIdRouter);
-
+const petIdRouter = Router({ mergeParams: true });
+petsRouter.use('/:id(\\-?\\d+)', petIdRouter);
 petIdRouter.use(reqPetById);
-
 petIdRouter.route('/')
   .get((req, res) => res.json(req.pet));
 
-petsByUserRouter.use(resPetsByUser);
+// const petsByTypeRouter = Router();
+// petsRouter.use('/?type', petsByTypeRouter);
+// petsByTypeRouter.get('/', reqPetsByType);
 
-petsByUserRouter.route('/')
-  .get((req, res) => {
-    res.json();
-  });
+// const petsAgeMoreThenRouter = Router();
+// petsRouter.use('/?age_gt', petsAgeMoreThenRouter);
+// petsAgeMoreThenRouter.get('/', resUsersHasPetType);
+//
+// const petsAgeLessThenRouter = Router();
+// petsRouter.use('/?age_lt', petsAgeLessThenRouter);
+// petsAgeLessThenRouter.get('/', resPetsAgeLessThen);
 
 // idRouter.route('/:id (\\d)')
 //   .get((req, res) => {
@@ -204,7 +270,16 @@ petsByUserRouter.route('/')
 
 // app.use(logParams);
 
-app.use('/task3B', rootRouter);
+const petsPopulateRouter = Router();
+petsRouter.use('/populate', petsPopulateRouter);
+petsPopulateRouter.get('/', reqUsers, petsPopulate);
+
+const petPopulateRouter = Router();
+petIdRouter.use('/populate', petPopulateRouter);
+petPopulateRouter.get('/', reqUsers, petPopulate);
+
+
+app.use('/task3B', rootRouter, catchErrors);
 
 app.listen(config.port, async () => {
   console.log(`Your app listening on port ${config.port}!`);
---
+++ src/middlewares/users.js
@@ -0,0 +1,51 @@
+import _ from 'lodash';
+import Pets from './pets';
+
+class Users {
+  constructor(petsData) {
+    return petsData.users;
+  }
+}
+
+const getUserById = (id) => {
+  if (!id) throw new Error('!id');
+  const user = _.find(this, usr => (usr.id === id));
+  if (!user) throw new Error('!user');
+  return user;
+};
+
+const getUsersHavePetType = (petsType) => {
+  const pets = Pets.getPetsByType(petsType);
+  let usersHavePetType = pets.map(pet =>
+    _.find(this, user => (user.id === pet.userId)));
+  // if (!usersHavePetType) throw new Error('!usersHavePetType');
+  usersHavePetType = _(usersHavePetType).sortBy(['id']).sortedUniq();
+  return usersHavePetType;
+}
+
+const getUserByName = (username) => {
+  // if (!username) next('!username');
+  const user = _.find(this, obj => (obj.username === username));
+  // if (!user) next('!user');
+  return user;
+};
+
+const populateUsers = (pets) => {
+  const usersPop = this.reduce((usersP, user) => {
+    let newUser = _.clone(user);
+    newUser.pets = pets.filter(pet => (user.id === pet.userId));
+    if (newUser.pets.length > 0)
+      usersP.push(newUser);
+    return usersP;
+  }, []);
+  // if (!usersPop) next('!usersPop');
+  return usersPop;
+};
+
+const populateUser = (pets, userId) => {
+  let user = _.clone(getUserById(userId));
+  user.pets = pets.filter(pet => (user.id === pet.userId));
+  // if (user.pets.length === 0)
+  //   next('user does not have pet');
+  return user;
+};
