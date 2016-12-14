a7261cb2b00aeb0a37aad7895e7416d4be615a0a
Anton Vasiliev <vasilievpost@gmail.com>
2016-12-15 00:24:26 +0500

    Pets model done s :

--- src/models/pets.js
+++ src/models/pets.js
@@ -5,41 +5,49 @@ export default class Pets {
     return petsData.pets;
   }
 
-  const getPetsByType = (typePet => {
-    if (!typePet) throw new Error('!typePet');
+  filterByType(typePet) {
+    if (!typePet) throw new Error('!typePet'); // нужно ли тут генерить ошибку что нет оператора?
     const petsByType = this.filter(pet => (pet.type === typePet));
-    if (!petsByType) throw new Error('!petsByType');
+    if (!petsByType) throw new Error('!petsByType'); // нужно ли тут генерить ошибку что нет животных с таким типом или отдавать андеф?
     return petsByType;
-  });
+  }
 
-  const getPetsAgeMoreThen = (age_gt => {
+  filterAgeMoreThen(age_gt) {
     if (!age_gt) throw new Error('!age_gt');
     const petsAgeMoreThen = this.filter(pet => (pet.age > age_gt));
     if (!petsAgeMoreThen) throw new Error('!petsAgeMoreThen');
-  });
+    return petsAgeMoreThen;
+  }
 
-  const getPetsAgeLessThen = (age_lt =>
-    this.filter(pet => (pet.age < age_lt))
-  );
+  filterAgeLessThen(age_lt) {
+		if (!age_lt) throw new Error('!age_lt');
+    const petsAgeLessThen = this.filter(pet => (pet.age < age_lt));
+		if (!petsAgeLessThen) throw new Error('!petsAgeLessThen');
+		return petsAgeMoreThen;
+	}
 
-  const getPetById = (id =>
-    _.find(this, obj => (obj.id === id))
-  );
+  filterByUser(user) {
+    return this.filter(pet => (pet.userId === user.id))
+  }
 
-  const getPetsByUser = (user =>
-    this.filter(pet => (pet.userId === user.id))
-  );
+  populate(users) {
+    const populatedPets = this.map((pet) => {
+      const user = _.find(users, user => (user.id === pet.userId));
+      return { ...pet, user };
+    });
+		if (!populatedPets) throw new Error('!populatedPets');
+		return populatedPets;
+  }
 
-  const populates = (users =>
-    this.map((pet) => {
-      let newPet = _.clone(pet);
-      newPet.user = _.find(users, user => (user.id === pet.userId));
-      return newPet;
-    })
-  );
+	getOneById(id) {
+		return _.find(this, obj => (obj.id === id))
+	}
 
-  const populate = (users, petId) => {
-    const pet = getPetById(petId);
-    return _.find(users, user => (user.id === pet.userId));
-  };
+	populateOne(users, petId) {
+	  const pet = getOneById(petId);
+		if (!pet) throw new Error('!pet');
+		const user = _.find(users, currentUser => (currentUser.id === pet.userId));
+	  if (!user) throw new Error('!user');
+	  return { ...pet, user };
+	}
 }
