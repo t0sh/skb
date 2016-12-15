e360d120ae9c84089221322de04cab382343667b
Anton Vasiliev <vasilievpost@gmail.com>
2016-12-15 23:26:21 +0500

    Users model done

--- src/models/users.js
+++ src/models/users.js
@@ -2,51 +2,68 @@
 import _ from 'lodash';
 import Pets from './pets';
 
-class Users {
+export default class Users {
   constructor(petsData) {
     return petsData.users;
   }
+
+	getOneById(id) {
+	  if (!id) throw new Error('!id');
+	  const user = _.find(this, usr => (usr.id === id));
+	  if (!user) throw new Error('!user');
+	  return user;
+	}
+
+	filterHavePetType(petsType) {
+		const pets = Pets.filterByType(petsType);
+		const usersHavePetType = pets.map(pet =>
+			_.find(this, user => (user.id === pet.userId)));
+	  if (!usersHavePetType) throw new Error('!usersHavePetType');
+		return _(usersHavePetType).sortBy(['id']).sortedUniq();
+	}
+
+	getOneByName(username) {
+	  if (!username) throw new Error('!username');
+	  const user = _.find(this, obj => (obj.username === username));
+	  if (!user) throw new Error('!user');
+	  return user;
+	}
+
+	static populate(pets, users) {
+		const populatedUsers = users.reduce((resultUsers, currentUser) => {
+			const userPets = pets.filter(pet => (currentUser.id === pet.userId));
+			if (!userPets.isEmpty) {
+	      resultUsers.push({ ...currentUser, pets: userPets });
+	    }
+	    return resultUsers;
+	  }, []);
+	  if (!populatedUsers) throw new Error('!populatedUsers');
+	  return populatedUsers;
+	}
+
+	populate(pets) {
+		const populatedUsers = this.populate(pets, this);
+	  return populatedUsers;
+	}
+
+	populateOne(pets, userId) {
+	  const user = getOneById(userId);
+	  const userPets = pets.filter(pet => (user.id === pet.userId));
+	  if (pets.isEmpty) throw new Error('user does not have pet');
+	  return { ...user, pets: userPets };
+	}
+
+	// populateUsersHavePetType(pets, petType) {
+	// 	const populatedUsers = populate(pets);
+	// 	const populatedUsersHavePetType =
+	// 		populatedUsers.filter(user => (user.pets.some(pet => (pet.type === petType))));
+	// 	if (!populatedUsersHavePetType) throw new Error('!populatedUsersHavePetType');
+	// 	return populatedUsersHavePetType;
+	// }
+
+	populateUsersHavePetType(pets, petType) {
+		const usersHasPetType = this.filterHavePetType(petType);
+		const populatedUsersHavePetType = this.populate(pets, usersHasPetType);
+		return populatedUsersHavePetType;
+	}
 }
-
-const getUserById = (id) => {
-  if (!id) throw new Error('!id');
-  const user = _.find(this, usr => (usr.id === id));
-  if (!user) throw new Error('!user');
-  return user;
-};
-
-const getUsersHavePetType = (petsType) => {
-  const pets = Pets.getPetsByType(petsType);
-  let usersHavePetType = pets.map(pet =>
-    _.find(this, user => (user.id === pet.userId)));
-  // if (!usersHavePetType) throw new Error('!usersHavePetType');
-  usersHavePetType = _(usersHavePetType).sortBy(['id']).sortedUniq();
-  return usersHavePetType;
-}
-
-const getUserByName = (username) => {
-  // if (!username) next('!username');
-  const user = _.find(this, obj => (obj.username === username));
-  // if (!user) next('!user');
-  return user;
-};
-
-const populateUsers = (pets) => {
-  const usersPop = this.reduce((usersP, user) => {
-    let newUser = _.clone(user);
-    newUser.pets = pets.filter(pet => (user.id === pet.userId));
-    if (newUser.pets.length > 0)
-      usersP.push(newUser);
-    return usersP;
-  }, []);
-  // if (!usersPop) next('!usersPop');
-  return usersPop;
-};
-
-const populateUser = (pets, userId) => {
-  let user = _.clone(getUserById(userId));
-  user.pets = pets.filter(pet => (user.id === pet.userId));
-  // if (user.pets.length === 0)
-  //   next('user does not have pet');
-  return user;
-};
